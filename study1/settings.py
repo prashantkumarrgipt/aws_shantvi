18d129c78ee4036a4ec685ee29e815a9c2d032cf
prashantkumarrgipt <99869977+prashantkumarrgipt@users.noreply.github.com>
2022-11-24 16:37:35 +0530

    backa

--- study1/settings.py
+++ study1/settings.py
@@ -133,16 +133,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 if DEBUG==True:
     STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+    STATIC_ROOT = "/home/ubuntu/shantvi/aws_shantvi/aws_shantvi/static"
     # STATICFILES_DIRS =["/home/ubuntu/shantvi/aws_shantvi/aws_shantvi/static"]
 
 #     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-    
+
 
 # MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 
