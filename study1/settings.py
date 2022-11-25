33a9aa7aac2cb7bb958a38051c66ef3338f539b1
prashantkumarrgipt <99869977+prashantkumarrgipt@users.noreply.github.com>
2022-11-25 17:26:32 +0530

    media template

--- study1/settings.py
+++ study1/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.template.context_processors.media',
 ]
 
 ROOT_URLCONF = 'study1.urls'
